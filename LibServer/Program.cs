1a1e0bd24fe704680e9a8c0da3dea2c6bad9c7ee
Rj <horawalarojitha@gmail.com>
2025-09-21 22:34:26 -0700

    Add project files.

---
+++ LibServer/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer(); // For minimal APIs in .NET 6+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+}
+
+app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ LibServer/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace LibServer
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int Author { get; set; }
+
+        public int CoAuthor => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
